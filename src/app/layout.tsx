7f891bb96fca47b274e822ed4a3652aef3ba1f87
ozet1001 <ozeki.10.1@gmail.com>
2025-08-17 23:05:27 +0900

    iOS で input の focus 時にズームインしてしまうのを viewport で解決する
    https://zenn.dev/rhirayamaaan/articles/f0209ad6574ed4

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,4 @@
-import type { Metadata } from "next";
+import type { Metadata, Viewport  } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 
@@ -32,6 +32,13 @@ export const metadata: Metadata = {
     "エイミー",
   ],
 };
+
+export const viewport: Viewport = {
+  width: 'device-width',
+  initialScale: 1,
+  maximumScale: 1,
+  userScalable: false,
+}
 
 export default function RootLayout({
   children,
