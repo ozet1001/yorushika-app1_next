23b2f0890ca9f8d40d7523ed279e8c78d43de981
ozet1001 <ozeki.10.1@gmail.com>
2025-09-15 17:20:22 +0900

    アナリティクスのタグ追加

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata, Viewport  } from "next";
+import Script from 'next/script'
 import localFont from "next/font/local";
 import "./globals.css";
 
@@ -47,6 +48,21 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+          {/* Google Analytics */}
+          <Script
+            src="https://www.googletagmanager.com/gtag/js?id=G-KJ3G5HC4CL"
+            strategy="afterInteractive"
+          />
+          <Script id="google-analytics" strategy="afterInteractive">
+            {`
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-KJ3G5HC4CL');
+            `}
+          </Script>
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
