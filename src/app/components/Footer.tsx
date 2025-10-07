8ed5a6ad81806a497acaffe5ad073f6ca585a41f
ozet1001 <ozeki.10.1@gmail.com>
2025-10-08 00:48:49 +0900

    プライバシーポリシーのページを新規作成

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -2,11 +2,19 @@
 function Footer() {
   return (
     <footer className="w-full py-5 text-white shadow-md" style={{ backgroundColor: '#499FD8' }}>
-        <div className="container flex flex-col items-center justify-center px-4 mx-auto">
-            <h1 className="mt-4 text-xl font-bold" style={{fontFamily: 'var(--font-kaisei), serif'}}>
-              月猫図書館
-            </h1>
+      <div className="container flex flex-col items-center justify-center px-4 mx-auto">
+        <p className="text-xl font-bold" style={{fontFamily: 'var(--font-kaisei), serif'}}>
+          月猫図書館
+        </p>
+        <p className="mt-3 text-xs sm:text-sm text-center">
+          ※ 本サイトはファンによる個人運営であり、公式とは一切関係ありません
+        </p>
+        <div className="mt-1 text-xs sm:text-sm space-x-4">
+          <a href="/privacy" className="hover:text-blue-100 underline transition-colors">
+            プライバシーポリシー
+          </a>
         </div>
+      </div>
     </footer>
   )
 }
